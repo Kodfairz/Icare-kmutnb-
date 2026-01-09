3f72f8fe6e499e953d0720aab084ed881f477f51
Chayathon.T <gusgus157903@gmail.com>
2026-01-10 00:27:36 +0700

    Update api.ts

--- frontend/src/app/service/api.ts
+++ frontend/src/app/service/api.ts
@@ -1,2 +1,3 @@
- export const API ="http://localhost:4000";
-//export const API ="http://202.44.47.45:4000";
+// export const API ="http://localhost:4000";
+// export const API ="http://202.44.47.45:4000";
+export const API = "https://icare-kmutnb-production.up.railway.app";
